a0ec47286835cf3fe2c1c68d27cbaf6cb12efc5b
flaviorangel <flm.rangel@gmail.com>
2022-03-23 21:50:42 -0300

    feat: add exercise 2

--- exercicio1_2_matrizes/exercicio1_2.c
+++ exercicio1_2_matrizes/exercicio1_2.c
@@ -3,43 +3,50 @@
 #include <stdlib.h>
 #include <time.h>
 
-int matriz_identidade (int* mat, int n){
+int matriz_identidade (int** mat, int n){
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
             if (i == j){
-                if (*(mat + (i * n) + j) != 1) return 0;
+                if (mat[i][j] != 1){
+                    return 0;
+                }
             } else {
-                if (*(mat + (i * n) + j) != 0) return 0;
+                if (mat[i][j] != 0){
+                    return 0;
+                }
             }
         }
     }
     return 1;
 }
 
-int* cria_matriz(int n, int eh_identidade){
-    int* mat = (int*)malloc(sizeof(int)*n*n);
+int** cria_matriz(int n, int eh_identidade){
+    int** mat = (int**)malloc(sizeof(int*)*n);
+    for (int i = 0; i < n; i++){
+        mat[i] = (int*)malloc(sizeof(int)*n);
+    }
     if (eh_identidade == 1) {
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                if (i == j) mat[(i * n) + j] = 1;
-                else mat[(i * n) + j] = 0;
+                if (i == j) mat[i][j] = 1;
+                else mat[i][j] = 0;
             }
         }
     } else {
         srand(time(NULL));
         for (int i = 0; i < n; i++){
             for (int j = 0; j < n; j++){
-                mat[(i * n) + j] = (rand() % (101) + 1);
+                mat[i][j] = (rand() % (101) + 1);
             }
         }
     }
     return mat;
 }
 
-void imprime_matriz(int* mat, int n){
+void imprime_matriz(int** mat, int n){
     for (int i = 0; i < n; i++){
         for (int j = 0; j < n; j++){
-            printf("(%d, %d) = %d\n", i, j, *(mat + (i * n) + j));
+            printf("(%d, %d) = %d\n", i, j, mat[i][j]);
         }
     }
 }
