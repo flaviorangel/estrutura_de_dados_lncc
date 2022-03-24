31851be5b0012cc04d373d7c558fbfecee20f3a1
flaviorangel <flm.rangel@gmail.com>
2022-03-23 21:00:12 -0300

    feat: add exercise 1.1

---
+++ exercicio1_1_matrizes/exercicio1_1.c
@@ -0,0 +1,45 @@
+#include "exercicio1_1.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int matriz_identidade (int* mat, int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (i == j){
+                if (*(mat + (i * n) + j) != 1) return 0;
+            } else {
+                if (*(mat + (i * n) + j) != 0) return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int* cria_matriz(int n, int eh_identidade){
+    int* mat = (int*)malloc(sizeof(int)*n*n);
+    if (eh_identidade == 1) {
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                if (i == j) mat[(i * n) + j] = 1;
+                else mat[(i * n) + j] = 0;
+            }
+        }
+    } else {
+        srand(time(NULL));
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                mat[(i * n) + j] = (rand() % (101) + 1);
+            }
+        }
+    }
+    return mat;
+}
+
+void imprime_matriz(int* mat, int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            printf("(%d, %d) = %d\n", i, j, *(mat + (i * n) + j));
+        }
+    }
+}
